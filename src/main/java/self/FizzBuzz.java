8e672bff208ee50d14fc74feb6dc91e6c9996018
bigtonysayshi <panda90926@gmail.com>
2016-10-30 11:52:13 -0700

    add src dir

---
+++ src/main/java/self/FizzBuzz.java
@@ -0,0 +1,13 @@
+package self;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tzhang2 on 10/30/16.
+ */
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        return new ArrayList<String>();
+    }
+}
